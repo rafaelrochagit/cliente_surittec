2144d4915cbdc208fc19462bfe002aca3143de84
Rafael Rocha <rafaeltbt@gmail.com>
2020-01-14 00:11:12 -0300

    Inicializando projeto

---
+++ src/main/java/com/cliente/surittec/ClienteSurittecApplication.java
@@ -0,0 +1,13 @@
+package com.cliente.surittec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteSurittecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteSurittecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cliente/surittec/ClienteSurittecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliente.surittec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteSurittecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
