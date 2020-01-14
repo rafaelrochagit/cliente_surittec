091fa2a1104c0528a7638b1072cca6721639751d
Rafael Rocha <rafaeltbt@gmail.com>
2020-01-14 02:06:18 -0300

    Criando usuarios admin e comum

--- src/main/java/com/cliente/surittec/ClienteSurittecApplication.java
+++ src/main/java/com/cliente/surittec/ClienteSurittecApplication.java
@@ -1,13 +1,32 @@
 package com.cliente.surittec;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ClienteSurittecApplication {
+import com.cliente.surittec.domain.Usuario;
+import com.cliente.surittec.repositories.UsuarioRepository;
 
+@SpringBootApplication
+public class ClienteSurittecApplication implements CommandLineRunner {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteSurittecApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Usuario usuario1 = new Usuario(null, "admin", "123456");
+		Usuario usuario2 = new Usuario(null, "comum", "123456");
+		
+		usuarioRepository.saveAll(Arrays.asList(usuario1, usuario2));
 	}
 
 }
